bba0cce655be94ef800b55f29db2ab4ff84f40a8
Bobby <zhang.deplives@hotmail.com>
2021-12-10 14:06:08 +0800

    优化脚本

--- first.sh
+++ first.sh
@@ -3,9 +3,9 @@
 
 # Run before feeds install
 
-COMMIT_HASH=$1
-if [ -z "$COMMIT_HASH" ]; then
-    COMMIT_HASH='?'
+COMMIT_ID=$1
+if [ -z "$COMMIT_ID" ]; then
+    COMMIT_ID='?'
 fi
 
 # Modify default timezone
@@ -54,7 +54,7 @@ sed -i 's/<td id="wan6_i" style="width:16px; text-align:center; padding:3px">/<t
 # Add Firmware Commit Hash in Homepage
 echo 'Add Firmware Commit Hash in Homepage...'
 line_kernel_version=$(grep -n 'Kernel Version' package/lean/autocore/files/x86/index.htm | awk -F ':' '{print $1}')
-sed -i "${line_kernel_version}a\                <tr><td width=\"33%\"><%:Build Version%></td><td>$COMMIT_HASH</td></tr>" package/lean/autocore/files/x86/index.htm
+sed -i "${line_kernel_version}a\                <tr><td width=\"33%\"><%:Build Version%></td><td>$COMMIT_ID</td></tr>" package/lean/autocore/files/x86/index.htm
 
 # Add Build Date in Homepage
 echo 'Add Build Date in Homepage...'
@@ -85,6 +85,6 @@ echo "╚██████╔╝██║     ███████╗██║
 echo " ╚═════╝ ╚═╝     ╚══════╝╚═╝  ╚═══╝ ╚══╝╚══╝ ╚═╝  ╚═╝   ╚═╝    " >>package/base-files/files/etc/banner
 echo " ------------------------------------------------------------- " >>package/base-files/files/etc/banner
 echo " %D %C ${build_date} @Bobby                                    " >>package/base-files/files/etc/banner
-echo " $COMMIT_HASH                                                  " >>package/base-files/files/etc/banner
+echo " $COMMIT_ID                                                  " >>package/base-files/files/etc/banner
 echo " ------------------------------------------------------------- " >>package/base-files/files/etc/banner
 echo "                                                               " >>package/base-files/files/etc/banner
