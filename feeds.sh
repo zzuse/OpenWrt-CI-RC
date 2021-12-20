34351be4f83d73aad862c70a06255c8c123f6cda
Bobby <zhang.deplives@hotmail.com>
2021-12-20 13:14:26 +0800

    update

--- feeds.sh
+++ feeds.sh
@@ -8,8 +8,15 @@
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
+MODE=$1 # stable test
+
+BRANCH='dev'
+if [ "$MODE" == "stable" ]; then
+    BRANCH='main'
+fi
+
 echo 'Add helloworld feed...'
-sed -i '$a src-git helloworld https://github.com/deplives/helloworld;dev' feeds.conf.default
+sed -i "$ a src-git helloworld https://github.com/deplives/helloworld;$BRANCH" feeds.conf.default
 
 echo 'Add openwrt-package feed...'
-sed -i '$a src-git bobby https://github.com/deplives/openwrt-package;dev' feeds.conf.default
+sed -i "$ a src-git bobby https://github.com/deplives/openwrt-package;$BRANCH" feeds.conf.default
