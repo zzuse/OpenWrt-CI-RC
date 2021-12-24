4820cd5e8ac3efd6f7a7e5ab22c4d28a4be64f12
Bobby <zhang.deplives@hotmail.com>
2021-12-24 14:57:05 +0800

    替换 feeds packages 和 luci

--- feeds.sh
+++ feeds.sh
@@ -10,13 +10,21 @@
 
 MODE=$1 # stable test
 
-BRANCH='dev'
+BRANCH_BOBBY='dev'
+BRANCH_LEDE='dev'
 if [ "$MODE" == "stable" ]; then
-    BRANCH='main'
+    BRANCH_BOBBY='main'
+    BRANCH_LEDE='master'
 fi
 
+echo 'Replace https://github.com/coolsnowwolf/packages'
+sed -i "s/https:\/\/github.com\/coolsnowwolf\/packages/https:\/\/github.com\/deplives\/packages;$BRANCH_LEDE/g" feeds.conf.default
+
+echo 'Replace https://github.com/coolsnowwolf/luci'
+sed -i "s/https:\/\/github.com\/coolsnowwolf\/luci/https:\/\/github.com\/deplives\/luci;$BRANCH_LEDE/g" feeds.conf.default
+
 echo 'Add helloworld feed...'
-sed -i "$ a src-git helloworld https://github.com/deplives/helloworld;$BRANCH" feeds.conf.default
+sed -i "$ a src-git helloworld https://github.com/deplives/helloworld;$BRANCH_BOBBY" feeds.conf.default
 
 echo 'Add openwrt-package feed...'
-sed -i "$ a src-git bobby https://github.com/deplives/openwrt-package;$BRANCH" feeds.conf.default
+sed -i "$ a src-git bobby https://github.com/deplives/openwrt-package;$BRANCH_BOBBY" feeds.conf.default
