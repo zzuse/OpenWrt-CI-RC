0546568808ad804c8c2a477494ef2a4e9ee17dac
zzuse <zzuse@outlook.com>
2022-06-05 12:52:38 +0800

    for ssrp

--- first.sh
+++ first.sh
@@ -30,12 +30,12 @@ sed -i "s/https:\/\/github.com\/coolsnowwolf\/packages/https:\/\/github.com\/dep
 echo 'Replace https://github.com/coolsnowwolf/luci'
 sed -i "s/https:\/\/github.com\/coolsnowwolf\/luci/https:\/\/github.com\/deplives\/luci;$BRANCH_LEDE/g" feeds.conf.default
 
-echo 'Add helloworld feed...'
-sed -i "$ a src-git helloworld https://github.com/deplives/helloworld;$BRANCH_BOBBY" feeds.conf.default
+# echo 'Add helloworld feed...'
+# sed -i "$ a src-git helloworld https://github.com/deplives/helloworld;$BRANCH_BOBBY" feeds.conf.default
 
 echo 'Add openwrt-package feed...'
 sed -i "$ a src-git bobby https://github.com/deplives/openwrt-package;$BRANCH_BOBBY" feeds.conf.default
 
-#echo 'Add kenzok8...'
-#sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-#sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+echo 'Add kenzok8...'
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
